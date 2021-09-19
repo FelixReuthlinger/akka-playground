12c8254b12521c57c5fb82548d5775b60d91942c
Felix Reuthlinger <10428747+FelixReuthlinger@users.noreply.github.com>
2021-09-19 18:04:38 +0200

    Update RunnableAkkaApp.scala

--- src/main/scala/base/RunnableAkkaApp.scala
+++ src/main/scala/base/RunnableAkkaApp.scala
@@ -1,44 +1,43 @@
 package base
 
 import akka.actor.ActorSystem
-import akka.stream.scaladsl.Tcp.ServerBinding
 import akka.stream.scaladsl.{RunnableGraph, Tcp}
 
 import scala.concurrent.{ExecutionContextExecutor, Future}
 
-trait SimpleRunnableAkkaApp extends App {
+trait SimplerClassName {
+  def getSimplerClassName = getClass.getSimpleName.replaceAll("[^\\w]", "")
+}
+
+trait SimpleActorSystem extends SimplerClassName {
+  implicit val system: ActorSystem = ActorSystem(getSimplerClassName)
+  implicit val ec: ExecutionContextExecutor = system.dispatcher
+}
+
+trait SimpleRunnableAkkaApp extends App with SimpleActorSystem {
 
   def runIt: Future[_]
 
-  implicit val system: ActorSystem = ActorSystem(getClass.getSimpleName.replaceAll("[^\\w]", ""))
-  implicit val ec: ExecutionContextExecutor = system.dispatcher
   runIt.onComplete(_ => system.terminate())
 }
 
-trait PrintResultRunnableAkkaApp extends App {
+trait PrintResultRunnableAkkaApp extends App with SimpleActorSystem {
 
   def runIt: Future[Any]
 
-  implicit val system: ActorSystem = ActorSystem(getClass.getSimpleName.replaceAll("[^\\w]", ""))
-  implicit val ec: ExecutionContextExecutor = system.dispatcher
   val result: Future[Any] = runIt
   result.foreach(c => println(s"Result: ${c.toString}"))
   result.onComplete(_ => system.terminate())
 }
 
-trait GraphRunnableAkkaApp extends App {
+trait GraphRunnableAkkaApp extends App with SimpleActorSystem {
   def buildGraph: RunnableGraph[_]
 
-  implicit val system: ActorSystem = ActorSystem(getClass.getSimpleName.replaceAll("[^\\w]", ""))
-  implicit val ec: ExecutionContextExecutor = system.dispatcher
   buildGraph.run()
 }
 
-trait TcpRunnableAkkaApp extends App {
+trait TcpRunnableAkkaApp extends App with SimpleActorSystem {
   def runIt: Future[Tcp.OutgoingConnection]
-
-  implicit val system: ActorSystem = ActorSystem(getClass.getSimpleName.replaceAll("[^\\w]", ""))
-  implicit val ec: ExecutionContextExecutor = system.dispatcher
 
   val result: Future[Tcp.OutgoingConnection] = runIt
   //result.onComplete(_ => system.terminate())
